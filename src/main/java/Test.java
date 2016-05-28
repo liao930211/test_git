ad01b426016306229b977995c1e220a2e49c7d6c
liaohao <liao930211@163.com>
2016-05-28 10:23:13 +0800

    第一个测试类 提交

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+/**
+ * @author liaohao
+ * @ClassName Test
+ * @Description 类描述
+ * @date 2016/5/28
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("Test is Success！ ");
+    }
+
+
+}
